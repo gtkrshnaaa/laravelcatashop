be5c337a0d92b3c205149c00b608cf69e8a0c1e8
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-01-06 11:55:04 +0700

    Add customer routes: auth, dashboard, orders, addresses

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\Admin\Catalog\CategoryController;
 use App\Http\Controllers\Admin\Catalog\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\Order\TransactionController;
+use App\Http\Controllers\Customer\AddressController;
+use App\Http\Controllers\Customer\Auth\LoginController as CustomerLoginController;
+use App\Http\Controllers\Customer\Auth\RegisterController;
+use App\Http\Controllers\Customer\DashboardController as CustomerDashboardController;
+use App\Http\Controllers\Customer\OrderController;
 use App\Http\Controllers\Public\CartController;
 use App\Http\Controllers\Public\CatalogController;
 use App\Http\Controllers\Public\CheckoutController;
@@ -31,6 +36,40 @@ Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.s
 
 // Invoice Routes
 Route::get('/invoice/{transaction}', [InvoiceController::class, 'show'])->name('invoice.show');
+
+// Customer Routes
+Route::prefix('customer')->name('customer.')->group(function () {
+    // Guest only
+    Route::middleware('guest:customer')->group(function () {
+        Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+        Route::post('/register', [RegisterController::class, 'register']);
+        Route::get('/login', [CustomerLoginController::class, 'showLoginForm'])->name('login');
+        Route::post('/login', [CustomerLoginController::class, 'login']);
+    });
+
+    // Authenticated only
+    Route::middleware('auth:customer')->group(function () {
+        Route::post('/logout', [CustomerLoginController::class, 'logout'])->name('logout');
+        Route::get('/dashboard', [CustomerDashboardController::class, 'index'])->name('dashboard');
+        
+        // Orders
+        Route::prefix('orders')->name('orders.')->group(function () {
+            Route::get('/', [OrderController::class, 'index'])->name('index');
+            Route::get('/{transaction}', [OrderController::class, 'show'])->name('show');
+        });
+        
+        // Addresses
+        Route::prefix('addresses')->name('addresses.')->group(function () {
+            Route::get('/', [AddressController::class, 'index'])->name('index');
+            Route::get('/create', [AddressController::class, 'create'])->name('create');
+            Route::post('/', [AddressController::class, 'store'])->name('store');
+            Route::get('/{address}/edit', [AddressController::class, 'edit'])->name('edit');
+            Route::put('/{address}', [AddressController::class, 'update'])->name('update');
+            Route::delete('/{address}', [AddressController::class, 'destroy'])->name('destroy');
+            Route::post('/{address}/set-default', [AddressController::class, 'setDefault'])->name('setDefault');
+        });
+    });
+});
 
 // Admin Routes
 Route::prefix('admin')->name('admin.')->group(function () {
