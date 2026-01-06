aa41bc000538bc88107d1cfb72cca05f35a8214c
gtkrshnaaa <gtkrshnaaa@gmail.com>
2026-01-06 11:07:50 +0700

    Activate public routes for home, catalog, and product pages

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\Catalog\CategoryController;
 use App\Http\Controllers\Admin\Catalog\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Public\CatalogController;
+use App\Http\Controllers\Public\HomeController;
+use App\Http\Controllers\Public\ProductController as PublicProductController;
 use Illuminate\Support\Facades\Route;
 
-// Public Routes (will be implemented in Phase 5)
-Route::get('/', function () {
-    return view('welcome');
-});
+// Public Routes
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
+Route::get('/products/{product:slug}', [PublicProductController::class, 'show'])->name('products.show');
 
 // Admin Routes
 Route::prefix('admin')->name('admin.')->group(function () {
